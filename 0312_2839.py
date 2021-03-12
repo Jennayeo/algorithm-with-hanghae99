11c71b3b7af88ab9c36ef36db1884881f030ab25
Jenna <jiyoung.jenna.yeo@gmail.com>
2021-03-13 01:38:13 +0900

    이전 코드 error, 구글링 후 완성

--- 0312_2839.py
+++ 0312_2839.py
@@ -1,25 +1,12 @@
-kg = int(input())
-bag = 0
+sugar = int(input())
 
-if kg%5==0:
-    print(kg//5)
-elif kg%3==0:
-    a = ((kg//5) + ((kg % 5)//3))
-    b = (kg//3)
-    if a > b:
-        print(b)
-    else:
-        if a == int:
-            print(a)
-        else:
-            print(b)
-elif kg!=3 and kg<=7:
-    print(-1)
+bag = 0
+while sugar >= 0:
+    if sugar % 5 == 0:
+        bag += (sugar//5) #5로 나눈 몫이 가방 갯수
+        print(bag)
+        break
+    sugar -= 3 #5의 배수가 될때까지 -3해줌
+    bag += 1 #-3될때마다 가방 +1
 else:
-    while kg>=0:
-        if kg%5 != 0:
-            kg-=3 #3씩 빼주면서 5의배수로 만들어 줌
-            bag+=1 #설탕가방 +1해줌
-            if kg%5==0:
-                break
-    print(bag+kg//5)
+    print('-1')
